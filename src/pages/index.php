8a1f9e81c4e8f89cac5d04b20dcb3cf83bf578d1
delafjr <dela.fjr08@gmail.com>
2024-05-14 23:12:36 +0700

    gatau css biasalah, di tbn regis, padahal tadi bisa

--- src/pages/index.php
+++ src/pages/index.php
@@ -5,17 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Pengaduan</title>
+    <link rel="stylesheet" href="../styling/index.css">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"> -->
 </head>
 <body>
-    <div>
-        <a href="login.php">
-            <div class="btn">Masuk</div>
-        </a>
-        <a href="registration.php">
-            <div class="btn">Daftar</div>
-        </a>
+    <div class="container">
+        <!-- navbar -->
+        <div class="navbar">
+            <h2>Sistem Pengaduan</h2>
+            <a href="login.php" class="link">
+                <div class="btn-nav btn-login-regis">
+                    <p class="text-btn">Masuk</p>
+                </div>
+            </a>
+        </div>
+
+        <!-- title section -->
+        <div class="title-section">
+            <div class="title-content">
+                <h2>Lorem Ipsum</h2>
+                <div class="con-p">
+                    <p>Lorem ipsum dolor sit amet,</p>
+                    <p>consectetur adipiscing elit,</p>
+                    <p>sed do eiusmod tempor incididunt ut</p>
+                    <p>labore et dolore magna aliqua.</p>
+                </div>
+                <div class="con-btn">
+                    <a href="login.php" class="link">
+                        <div class="btn-login-regis btn-login">
+                            <p class="text-btn">Masuk</p>
+                        </div>
+                    </a>
+                    <a href="registration.php" class="link">
+                        <div class="btn-regis btn-login-regis">
+                            <p class="text-btn">Daftar</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="img-title">
+
+            </div>
+        </div>
     </div>
 </body>
 </html>
