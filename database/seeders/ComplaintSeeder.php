a11611f4e4af6da685bad10363152121418c0697
delafjr <dela.fjr08@gmail.com>
2024-10-11 13:51:02 +0700

    add(seed): add dummy content using seeder

---
+++ database/seeders/ComplaintSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ComplaintSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('complaints')->insert([
+            [
+                'category_id'           =>'1',
+                'user_id'               =>'2',
+                'content'               =>'Banyak sekali fasilitas umum di pantai ujungngeoro yang mengalami kerusakan',
+                'status'                =>'proses',
+                'date_of_complaint'     =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'category_id'           =>'2',
+                'user_id'               =>'4',
+                'content'               =>'Informasi yang disampaikan oleh petugas di wisata kebun teh Pagilaran tidak disampaikan secara profesional',
+                'status'                =>'proses',
+                'date_of_complaint'     =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'category_id'           =>'3',
+                'user_id'               =>'2',
+                'content'               =>'Sering terjadi tawuran disepanjang jalan di pantai Ujungnegoro ketika malam',
+                'status'                =>'selesai',
+                'date_of_complaint'     =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'category_id'           =>'4',
+                'user_id'               =>'5',
+                'content'               =>'Banyak sampah yang dibuang sembarangan di pantai Adinuso, Subah',
+                'status'                =>'proses',
+                'date_of_complaint'     =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'category_id'           =>'4',
+                'user_id'               =>'3',
+                'content'               =>'Banyak coretan yang tidak pantas disekitar alun-alun kota Batang',
+                'status'                =>'proses',
+                'date_of_complaint'     =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+        ]);
+    }
+}
---
+++ database/seeders/ResponseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ResponseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('responses')->insert([
+            [
+                'complaint_id'          =>'3',
+                'user_id'               =>'1',
+                'content'               =>'Terima kasih atas laporan Anda. Saat ini kami telah mengerahkan personil dari Polres Batang untuk menindaklanjuti masalah ini',
+                'date_of_response'      =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'complaint_id'          =>'5',
+                'user_id'               =>'1',
+                'content'               =>'Terima kasih atas laporan Anda. Saat ini kami telah mengerahkan personil dari Polres Batang untuk menindaklanjuti masalah ini',
+                'date_of_response'      =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                'complaint_id'          =>'2',
+                'user_id'               =>'1',
+                'content'               =>'Terima kasih atas laporan Anda. Kami akan segera memberikan edukasi lebih lanjut terhadap petugas wisata khususnya untuk wisata Kebuh Teh Pagilaran',
+                'date_of_response'      =>Carbon::now()->format('Y-m-d H:i:s')
+            ],
+        ]);
+    }
+}
