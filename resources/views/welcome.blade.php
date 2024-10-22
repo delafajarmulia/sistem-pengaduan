7a719bb9b8840b4160623bfc8a7891f2025085ae
delafjr <dela.fjr08@gmail.com>
2024-10-22 23:45:49 +0700

    [fix] : anomaly error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -37,7 +37,7 @@
                     Jika Anda menemukan kerusakan pada fasilitas pariwisata, jangan ragu untuk melaporkannya. Setiap laporan sangat berarti untuk meningkatkan pengalaman wisata di Batang. 
                     Bersama-sama, kita wujudkan pariwisata yang lebih baik!
                 </p> --}}
-                <p class="text-white-strong text-center font-2xl">
+                <p class="text-white-strong text-center font-2xl font-semibold">
                     Laporkan kerusakan fasilitas pariwisata di Kabupaten Batang! Setiap laporan Anda adalah langkah menuju pariwisata yang lebih baik dan aman bagi semua.
                 </p>
             </div>
@@ -51,34 +51,34 @@
         <div class="py-20 px-5 w-auto mx-auto md:w-3/4">
             <div class="mx-auto ">
                 <h1 class="font-bold text-4xl">
-                    Apa untungnya lapor?
+                    Komitmen kami
                 </h1>
-                <div class="grid grid-cols-2 flex justify-center items-center">
-                    <div class="flex flex-col">
-                        <img src="{{ asset('assets/icons/timer.svg') }}" alt="" class="w-12 mx-auto">
+                <div class="grid grid-cols-1 gap-6 mt-8 flex justify-center items-center md:grid-cols-2">
+                    <div class="flex flex-col text-center bg-gray bg-opacity-25 rounded-lg p-4 w-auto h-48 hover:shadow-xl shadow-gray">
+                        <img src="{{ asset('assets/icons/timer.svg') }}" alt="" class="w-12 h-12 mb-3 mx-auto mt-1">
                         <div>
-                            <p class="font-bold">Cepat dan Tepat</p>
+                            <p class="font-bold text-xl">Cepat dan Tepat</p>
                             <p>Menjamin respons cepat terhadap laporan.</p>
                         </div>
                     </div>
-                    <div class="flex flex-col">
-                        <img src="{{ asset('assets/icons/eye.svg') }}" alt="" class="w-12 mx-auto">
+                    <div class="flex flex-col text-center bg-gray bg-opacity-25 rounded-lg p-4 w-auto h-48 hover:shadow-xl shadow-gray">
+                        <img src="{{ asset('assets/icons/eye.svg') }}" alt="" class="w-12 h-12 mb-3 mx-auto">
                         <div>
-                            <p>Transparan</p>
+                            <p class="font-bold text-xl">Transparan</p>
                             <p>Menyediakan transparansi dalam proses perbaikan.</p>
                         </div>
                     </div>
-                    <div class="flex flex-col">
-                        <img src="{{ asset('assets/icons/chats.svg') }}" alt="" class="w-12 mx-auto">
+                    <div class="flex flex-col text-center bg-gray bg-opacity-25 rounded-lg p-4 w-auto h-48 hover:shadow-xl shadow-gray">
+                        <img src="{{ asset('assets/icons/chats.svg') }}" alt="" class="w-12 h-12 mb-3 mx-auto">
                         <div>
-                            <p>Partisipatif</p>
+                            <p class="font-bold text-xl">Partisipatif</p>
                             <p>Mengajak masyarakat untuk terlibat aktif dalam menjaga fasilitas pariwisata.</p>
                         </div>
                     </div>
-                    <div class="flex flex-col">
-                        <img src="{{ asset('assets/icons/seal-check.svg') }}" alt="" class="w-12 mx-auto">
+                    <div class="flex flex-col text-center bg-gray bg-opacity-25 rounded-lg p-4 w-auto h-48 hover:shadow-xl shadow-gray">
+                        <img src="{{ asset('assets/icons/seal-check.svg') }}" alt="" class="w-12 h-12 mb-3 mx-auto">
                         <div>
-                            <p>Aman dan Terpercaya</p>
+                            <p class="font-bold text-xl">Aman dan Terpercaya</p>
                             <p>Menyediakan sistem yang aman dan dapat diandalkan untuk melaporkan kerusakan dengan kerahasiaan terjaga.</p>
                         </div>
                     </div>
@@ -86,12 +86,28 @@
             </div>
         </div>
 
-        <p>Nikmati keindahan alam yang memikat, mulai dari pantai Ujungnegoro hingga air terjun Curug Genting. Kabupaten Batang di Jawa Tengah menawarkan pengalaman wisata yang tak terlupakan. 
-            Ayo jelajahi sekarang dan rasakan keindahannya!</p>
+        <div class="pt-0 pb-12 px-12 mx-auto md:w-3/4 md:pt-8 md:pb-24">
+            <p class="text-center text-xl font-semibold">
+                Laporkan kerusakan fasilitas pariwisata untuk bantu tingkatkan pengalaman wisata! 
+                Dengan satu laporan, Anda berkontribusi untuk pariwisata yang lebih baik di Kabupaten Batang!
+            </p>
+        </div>
 
-        @foreach ($spots as $spot)
+        <div class="pt-0 pb-12 px-5 mx-auto md:w-3/4 md:pt-8 md:pb-24">
+            <h1 class="font-bold text-4xl">
+                Wisata kami
+            </h1>
+        </div>
+
+        <details>
+            <summary>test</summary>
+            <p>Nikmati keindahan alam yang memikat, mulai dari pantai Ujungnegoro hingga air terjun Curug Genting. Kabupaten Batang di Jawa Tengah menawarkan pengalaman wisata yang tak terlupakan. 
+                Ayo jelajahi sekarang dan rasakan keindahannya!</p>
+        </details>
+
+        {{-- @foreach ($spots as $spot)
             <img src="{{ asset('spots/'.$spot->image) }}" alt="" style="width: 150px">
-        @endforeach
+        @endforeach --}}
 
         {{-- <script src="{{ mix('js/app.js') }}"></script> --}}
     </body>
