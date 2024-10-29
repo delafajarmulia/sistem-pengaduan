25f78a41e19080c3e8f1981ebdb35dd5af41638f
delafjr <dela.fjr08@gmail.com>
2024-10-29 19:54:44 +0700

    change response at test_application_response function at exampletest.php file

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,18 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
+    // public function test_the_application_returns_a_successful_response(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response = $this->get('/'); // Mengakses route '/'
+        $response->assertStatus(200); // Memastikan status 200
+        $response->assertSee('spots'); // Memastikan halaman mengandung kata "spots"
     }
+
 }
