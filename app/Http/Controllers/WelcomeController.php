ae60f9714448f1d0bef02adaca64d099cb92fba6
delafjr <dela.fjr08@gmail.com>
2024-10-29 16:24:23 +0700

    add error logging in WelcomeController@index method

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,12 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Spot;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
 
 class WelcomeController extends Controller
 {
     public function index()
     {
-        $spots = Spot::limit(3)->get();
-        return view('welcome', compact('spots')); // 
+        try {
+            // Logika untuk mengambil data
+            $spots = Spot::limit(3)->get();
+            return view('welcome', compact('spots')); // 
+            // $data = /* logika untuk mendapatkan data */;
+            // return view('home', compact('data'));
+        } catch (\Exception $e) {
+            log::error($e->getMessage());
+            return response()->json(['error' => 'Internal Server Error'], 500);
+        }
     }
 }
