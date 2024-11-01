6896aa3c9d702213a988fb1485ca5697c7166479
delafjr <dela.fjr08@gmail.com>
2024-11-01 18:51:22 +0700

    [fix] : UI for print report

--- app/Http/Controllers/PrintReportController.php
+++ app/Http/Controllers/PrintReportController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Complaint;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class PrintReportController extends Controller
 {
     public function index()
     {
-        return view('print-report');
+        $complaints = Complaint::with('responses')->with('user')->with('category')->with('spot')->with(['responses.user'])->latest()->get();
+        $datetimeNow = Carbon::now()->setTimeZone('Asia/Jakarta')->format('d-m-Y H:i:s');
+        return view('print-report', compact(['complaints', 'datetimeNow']));
     }
 }
--- resources/views/print-report.blade.php
+++ resources/views/print-report.blade.php
@@ -8,7 +8,50 @@
     @vite('resources/css/app.css')
 </head>
 <body>
-    <p class="text-bold">Hello world!</p>
+    <div class="mt-9 text-black">
+        <h1 class="text-2xl font-bold md:p-5 text-center">Semua Pengaduan</h1>
+        <p class="text-left pl-20 mt-5 mb-1">Dicetak pada : {{ $datetimeNow }}</p>
+        <div class="flex flex-col justify-center items-center">
+
+            @forelse ($complaints as $complaint)
+                <div class="w-4/5 rounded-md border border-gray m-3 p-5 md:p-7">
+                    <div class="flex flex-col md:flex-row justify-between">
+                        <div class="md:order-1">
+                            <h3 class="font-semibold text-lg">{{ censorName($complaint->user->name) }}</h3>
+                            <h6 class="text-xs">{{ $complaint->date_of_complaint }}</h6>
+                            <h5 class="font-semibold md:whitespace-nowrap">Lokasi : {{ $complaint->spot?->name }}</h5>
+                            <h5 class="font-semibold md:whitespace-nowrap">Kategori : {{ $complaint->category->name }}</h5>
+                            <h5 class="font-semibold pr-2 {{ $complaint->status == 'proses' ? 'text-yellow-light' : 'text-green-light'}}">Status : {{ $complaint->status }}</h5>
+                        </div>
+                    </div>
+                    
+                    @if ($complaint->image)
+                        <div class="flex justify-center items-center">
+                            <img 
+                                src="{{ asset('complaints/'.$complaint->image) }}" 
+                                alt="{{ $complaint->image }}"
+                                class="w-1/2 h-1/2 my-1"
+                            >
+                        </div>
+                    @endif
+                    
+                    <p>{{ $complaint->content }}</p>
+                    <hr class="mt-1.5 mb-1 border border-gray">
+                    @foreach ($complaint->responses()->get() as $response)
+                        <div class="pl-7">
+                            <h4 class="font-semibold">{{ $response->user->role === 'admin' ? $response->user->name : censorName($response->user->name) }}</h4>
+                            <p>{{ $response->content }}</p>
+                        </div>
+                    @endforeach
+                </div>
+            @empty
+                <p class="text-center">
+                    Belum ada laporan.
+                </p>
+            @endforelse
+        </div>
+
+    </div>
 
     <script>
         window.onload = function(){
